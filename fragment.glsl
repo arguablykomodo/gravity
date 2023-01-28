cc600e84a2c560ff7f15f1dcc0b2447e0d930db3
Komodo <noreply@komodo.ar>
2023-01-28 00:17:47 -0300

    feat: antialiased circles

--- fragment.glsl
+++ fragment.glsl
@@ -5,9 +5,11 @@ precision highp float;
 in vec2 v_velocity;
 in vec2 v_uv;
 
-out vec4 color;
+out vec4 out_color;
 
 void main() {
-  float mask = step(length(v_uv), 1.0);
-  color = mask * vec4(v_velocity / (2.0 + abs(v_velocity)) * 0.5 + 0.5, 1.0, 1.0);
+  float width = fwidth(length(v_uv));
+  float mask = 1.0 - smoothstep(1.0 - width, 1.0, length(v_uv));
+  vec3 color = vec3(v_velocity / (2.0 + abs(v_velocity)) * 0.5 + 0.5, 1.0);
+  out_color = mask * vec4(color, 1.0);
 }
