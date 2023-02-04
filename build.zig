7e67a7e444c29ee7c2a7a6dea59de7783551a300
Komodo <noreply@komodo.ar>
2023-02-04 20:46:13 -0300

    bench: add benchmark

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+    const optimize = b.standardOptimizeOption(.{});
 
     const wasm = b.addStaticLibrary(.{
         .name = "gravity",
@@ -32,4 +32,13 @@ pub fn build(b: *std.build.Builder) void {
     });
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&zig_tests.step);
+
+    const bench = b.addExecutable(.{
+        .name = "bench",
+        .root_source_file = .{ .path = "zig/bench.zig" },
+        .optimize = optimize,
+    });
+    const install_bench = b.addInstallArtifact(bench);
+    const bench_step = b.step("bench", "Build benchmark");
+    bench_step.dependOn(&install_bench.step);
 }
---
+++ zig/bench.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const Quadtree = @import("quadtree.zig").Quadtree;
+const Particle = @import("particle.zig").Particle;
+
+pub fn main() !void {
+    var allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer allocator.deinit();
+
+    var particles = try std.fmt.parseUnsigned(usize, std.os.getenv("PARTICLES").?, 10);
+    var steps = try std.fmt.parseUnsigned(usize, std.os.getenv("STEPS").?, 10);
+    const spread = try std.fmt.parseFloat(f32, std.os.getenv("SPREAD").?);
+    const speed = try std.fmt.parseFloat(f32, std.os.getenv("SPEED").?);
+
+    var quadtree = Quadtree.init(allocator.allocator());
+    defer quadtree.deinit();
+
+    var random = std.rand.DefaultPrng.init(0);
+    const rng = random.random();
+    while (particles > 0) : (particles -= 1) {
+        try quadtree.insert(Particle.new(
+            .{ (rng.float(f32) * 2.0 - 1.0) * spread, (rng.float(f32) * 2.0 - 1.0) * spread },
+            .{ (rng.float(f32) * 2.0 - 1.0) * speed, (rng.float(f32) * 2.0 - 1.0) * speed },
+            1.0,
+        ));
+    }
+
+    while (steps > 0) : (steps -= 1) try quadtree.step(1.0 / 60.0);
+}
